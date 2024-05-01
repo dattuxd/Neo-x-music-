627f8074c8f464bc812bac16823d5875032ac99e
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-01 23:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight_rider:GODGURU12345@k
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002055640243"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1001905286767"))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "6934171164"))
