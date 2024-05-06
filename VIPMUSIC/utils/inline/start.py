d81be9a8bbd220edb953b129936540704db0865e
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2024-05-06 14:39:21 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -33,7 +33,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text="𝙍𝙤𝙮𝙖𝙡𝙠𝙞𝙣𝙜", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="💖 𝙍𝙤𝙮𝙖𝙡𝙠𝙞𝙣𝙜 💖", url=config.SUPPORT_CHAT),
             InlineKeyboardButton(text="❥͜͡🇧‌𝐓𝐒༎ࠫ𝆺꯭𝅥", url=config.SUPPORT_CHANNEL),
         ],
         [
