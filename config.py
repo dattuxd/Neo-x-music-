3553775a2dc24886f71b80dac44c1b7137535e02
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2024-05-07 08:54:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/dattudd/Sky-x-music-",
+    "https://github.com/dattuxd/Neo-x-music-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
